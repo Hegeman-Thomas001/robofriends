5c0bf02bc923897577eab707f46db5ff6bdbb2db
Thomas Hegeman <hegemant001@gmail.com>
2023-01-13 14:54:25 -0500

    updated readme with image link

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-Robofriends
+Robofriends:
 
 Search for robotic friends from those given!
+
+Fetched from: https://jsonplaceholder.typicode.com/users
+
+- A fun api with more data to grab and include in your program.
+
+AND
+
+Using: https://robohash.org
+
+- https://robohash.org/somefunhashing.png?200x200
+- Result if api is working:
+  ![Robofriend:](https://robohash.org/somefunhashing.png?200x200)
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,50 +1,85 @@
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css";
-import { Component } from "react";
+// import { Component } from "react";
 
-// smart component, lifecycle methods, mounting, updating, unmounting
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: "",
-    };
-  }
+const App = () => {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((users) => this.setState({ robots: users }))
-      .catch((err) => console.err(err));
-  }
-
-  onSearchChange = (evt) => {
-    this.setState({ searchfield: evt.target.value });
+  const onSearchChange = (evt) => {
+    setSearchfield(evt.target.value);
   };
 
-  render() {
-    const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    });
-    return !robots.length ? (
-      <h1>Loading</h1>
-    ) : (
-      <section className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <ErrorBoundry>
-            <CardList robots={filteredRobots} />
-          </ErrorBoundry>
-        </Scroll>
-      </section>
-    );
-  }
-}
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => setRobots(users))
+      .catch((err) => console.error(err));
+  });
+
+  return !robots.length ? (
+    <h1>Loading</h1>
+  ) : (
+    <section className="tc">
+      <h1 className="f1">RoboFriends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <ErrorBoundry>
+          <CardList robots={filteredRobots} />
+        </ErrorBoundry>
+      </Scroll>
+    </section>
+  );
+};
+
+// // smart component, lifecycle methods, lifecycle: mounting, updating, unmounting
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       robots: [],
+//       searchfield: "",
+//     };
+//   }
+
+// componentDidMount() {
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((res) => res.json())
+//     .then((users) => this.setState({ robots: users }))
+//     .catch((err) => console.err(err));
+// }
+
+//   onSearchChange = (evt) => {
+//     this.setState({ searchfield: evt.target.value });
+//   };
+
+//   render() {
+//     const { robots, searchfield } = this.state;
+//     const filteredRobots = robots.filter((robot) => {
+//       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+//     });
+//     return !robots.length ? (
+//       <h1>Loading</h1>
+//     ) : (
+//       <section className="tc">
+//         <h1 className="f1">RoboFriends</h1>
+//         <SearchBox searchChange={this.onSearchChange} />
+//         <Scroll>
+//           <ErrorBoundry>
+//             <CardList robots={filteredRobots} />
+//           </ErrorBoundry>
+//         </Scroll>
+//       </section>
+//     );
+//   }
+// }
 
 export default App;
